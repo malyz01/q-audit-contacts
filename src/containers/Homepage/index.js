04dc9019117150ae31a40efaa6aa8f9c52b9755a
malyz <mattuy001@gmail.com>
2019-09-11 15:14:19 +1200

    initial commit

---
+++ src/containers/Homepage/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is home page</h1>
+        <form>
+          <input type="text"></input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Home;
