6f8c6ba9546bd450c3ffe71dd9cc6547c03f2e48
malyz <mattuy001@gmail.com>
2019-09-11 15:32:00 +1200

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "./containers/Homepage";
-import "./App.css";
 
 function App() {
   return (
--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -1,13 +1,32 @@
 import React from "react";
+import { FormControl, TextField, Button } from "@material-ui/core";
+
+import "./index.css";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>This is home page</h1>
-        <form>
-          <input type="text"></input>
-        </form>
+        <div className="container">
+          <FormControl>
+            <TextField
+              id="outlined-search"
+              label="Username"
+              type="text"
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              id="outlined-search"
+              label="Password"
+              type="password"
+              margin="normal"
+              variant="outlined"
+            />
+            <Button>Submit</Button>
+          </FormControl>
+        </div>
       </div>
     );
   }
